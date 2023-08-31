7a731f39efcb0e1301ddb711e4c0a2f40e73afbb
Jorge <jorge.toledo.6@hotmail.com>
2023-08-31 14:38:15 -0400

    Conceptos POO y Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,33 @@
+#MODELAR LAS CLASS (CLASES) DE LAS SIGUIENTES ENTIDADES
+
+#Boleta
+#Factura
+#Venta
+#Cliente
+#Vendedor
+#Producto 
+
+#Doctores
+#AtencionMedica
+#Examen
+#Medicamento
+#Asistente
+
+
+class Boleta:
+    pass
+    #Atributos
+
+    #Metodos
+
+class Factura:
+    pass
+    #Atributos
+
+    #Metodos
+
+class Venta:
+    pass
+    #Atributos
+
+    #Metodos
